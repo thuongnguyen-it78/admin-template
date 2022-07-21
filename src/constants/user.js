56e588009b72e392eb9534b525febbf62d04b420
Andres Nguyen <thuongnguyen.it78@gmail.com>
2022-07-22 00:34:19 +0700

    update status template

--- src/constants/user.js
+++ src/constants/user.js
@@ -1,6 +1,13 @@
-export const ACTIVE_STATUS = { id: 1, name: 'Active' }
-export const INACTIVE_STATUS = { id: -1, name: 'Inactive' }
-export const STATUSES = [ACTIVE, INACTIVE]
+// export const ACTIVE_STATUS = { id: 1, name: 'Active' }
+// export const INACTIVE_STATUS = { id: -1, name: 'Inactive' }
+// export const STATUSES = [ACTIVE, INACTIVE]
 
-// using 
+// using
 // import * as USER from 'constants/user'. USER.ACTIVE_STATUS. USER.STATUSES
+
+export const userStatus = {
+  active: { id: 1, name: 'Active' },
+  inactive: { id: -1, name: 'Inactive' },
+}
+
+export const userStatusList = Object.values(userStatus)
