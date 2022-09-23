52db970b541ec4be2539ae9780f7c28d13353e56
Andres Nguyen <thuongnguyen.it78@gmail.com>
2022-09-24 00:26:55 +0700

    update code

--- src/commons/CommonBreadcumb.js
+++ src/commons/CommonBreadcumb.js
@@ -1,14 +1,22 @@
 import { Breadcrumb } from 'antd'
 import { Link } from 'react-router-dom'
+import { PieChartOutlined } from '@ant-design/icons'
 
-const defaultRoutes = [{ path: '/dashboard', name: 'Dashboard' }]
+const defaultRoutes = [{ path: '/dashboard', name: 'Dashboard', icon: <PieChartOutlined /> }]
 
 function CommonBreadcrumb({ routes }) {
   const renderBreadcrumbItems = (routes) => {
     const newRoutes = [...defaultRoutes, ...routes]
     return newRoutes.map((item, index) => {
+      const children = (
+        <span className="d-inline-flex align-items-center">
+          {item.icon && <span className="pe-1">{item.icon}</span>}
+          <span>{item.name}</span>
+        </span>
+      )
+
       if (item.active || item.disabled) {
-        return <Breadcrumb.Item key={index}>{item.name}</Breadcrumb.Item>
+        return <Breadcrumb.Item key={index}>{children}</Breadcrumb.Item>
       }
 
       return (
@@ -19,7 +27,7 @@ function CommonBreadcrumb({ routes }) {
               search: item.search,
             }}
           >
-            {item.name}
+            {children}
           </Link>
         </Breadcrumb.Item>
       )
